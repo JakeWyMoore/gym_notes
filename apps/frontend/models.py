75bee8a9920a163709c56d618b024aa0c1e1753b
Jacob Moore <jakewymoore@gmail.com>
2021-12-19 19:58:06 -0700

    user models

--- apps/frontend/models.py
+++ apps/frontend/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+from sre_constants import error
+from django.db import models
+
+import bcrypt
+import re
+
+class UserManager(models.Manager):
+
+    # REGISTER VALIDATIONS
+    def register_validator(self, postData):
+        errors = {}
+
+        if len(postData['first_name']) < 1:
+            errors['first_name'] = "Must enter first name."
+        if len(postData['last_name']) < 1:
+            errors['last_name'] = "Must enter last name."
+
+        email_regex = re.compile(r'^[a-zA-Z0-9.=_-]+@[a-zA-Z0-9.=_-]+\.[a-zA-Z]+$')
+
+        if len(postData['email']) == 0:
+            errors['email'] = "Must enter email."
+        elif not email_regex.match(postData['email']):
+            errors['email'] = "Must be valid email."
+
+    #LOGIN VALIDATIONS
+    def login_validator(self, postData):
+        errors = {}
+
+        existing_user = User.objects.filter(email = postData['email'])
+        if len(existing_user) != 1:
+            errors['email'] = "User does not exist"
+        if len(postData['email']) == 0:
+            errors['email'] = "No email entered"
+
+        if len(postData['password']) < 8:
+            errors ['password'] = "Password must be at least 8 characters"
+        elif bcrypt.checkpw(postData['password'].encode(), existing_user[0].password.encode()) != True:
+            errors['mismatch'] = "Email and password do not match"
+
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
