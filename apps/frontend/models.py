1f27926dd108bc061b968f218251ceb3ec9eda04
Jacob Moore <jakewymoore@gmail.com>
2022-01-08 20:30:14 -0700

    new workout html doc

--- apps/frontend/models.py
+++ apps/frontend/models.py
@@ -49,10 +49,11 @@ class User(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 class workout(models.Model):
+    group = models.CharField(max_length=50)
+
     sets = models.CharField(max_length=50)
     reps = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
-    group = models.CharField(max_length=50)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
